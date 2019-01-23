35d90f735e374b0d7e503fa24054f76658936950
Victor Ribeiro <victorqribeiro@gmail.com>
2019-01-23 04:02:09 -0200

    First commit

---
+++ goSpellcheck.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"os"
+	f "fmt"
+	io "io/ioutil"
+	s "strings"
+)
+
+func read_dic(file string) []string {
+	b, err := io.ReadFile(file)
+	if err != nil {
+		f.Println(err)
+	}
+	return s.Fields(string(b))
+}
+
+func distance(word_a string, word_b string) uint {
+	word_min := word_a
+	word_max := word_b
+	if( len(word_a) > len(word_b) ){
+		word_min = word_b
+		word_max = word_a
+	}
+	var dist uint = 0
+	for i := 0; i < len(word_min); i++ {
+		if word_min[i] != word_max[i] {
+			dist += 1
+		}
+	}
+	if len(word_min) != len(word_max) {
+		for i := 0; i < (len(word_max) - len(word_min)); i++ {
+			dist += 1
+		}
+	}
+	return dist
+}
+
+func inArray(a string, list []string) bool {
+    for _, b := range list {
+        if b == a {
+            return true
+        }
+    }
+    return false
+}
+
+func compare(word string, dic []string) []string {
+	sug := []string{"", "", ""}
+	min_distances := [3]uint{^uint(0), ^uint(0), ^uint(0)}
+	if !(inArray(word,dic)) {
+		for i := 0; i < len(dic); i++ {
+			d := distance(word,dic[i])
+			for j := 0; j < 3; j++ {
+				if d < min_distances[j] && !(inArray(dic[i],sug)) {
+					min_distances[j] = d
+					sug[j] = dic[i]
+				}
+			}
+		}
+	}
+	return sug
+}
+
+func main(){
+	
+	if len(os.Args) < 2 {
+		f.Println("Erro")
+		os.Exit(1)
+	}
+
+	word := string( os.Args[1] )
+	// dictionary from https://github.com/dwyl/english-words/blob/master/words.txt
+	dic := read_dic("words.txt")
+	result := compare(word,dic)
+
+	if len(result[0]) > 0 {
+		f.Println( s.Join(result, " ") )
+	} else {
+		f.Println("no errors")
+	}
+
+	
+}
---
+++ score.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+count=0
+t=0
+total=$(wc -l data.txt | cut -d " " -f1)
+while read line
+ do
+ 	t=$((t+1))
+ 	echo "$t from $total"
+  wrong=$( echo $line | cut -d ">" -f1 )
+  right=$( echo $line | cut -d ">" -f2 )
+  goresult=$(go run goSpellcheck.go $wrong | sed "s/'/\'/g")
+  suggestions=(${goresult})
+  for ((i=0; i<${#suggestions[@]}; i++))
+    do
+    if [[ ${suggestions[$i]} == $right ]]
+      then
+      count=$((count+1))
+      break
+ 	  fi
+ 	done
+ done < data.txt
+
+echo "got $count from $total"
