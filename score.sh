35a77bff544e9712c538816b3d57b5b4dad4b9ac
Victor Ribeiro <victorqribeiro@gmail.com>
2019-01-23 04:05:34 -0200

    fix identation

--- score.sh
+++ score.sh
@@ -5,8 +5,8 @@ t=0
 total=$(wc -l data.txt | cut -d " " -f1)
 while read line
  do
- 	t=$((t+1))
- 	echo "$t from $total"
+  t=$((t+1))
+  echo "$t from $total"
   wrong=$( echo $line | cut -d ">" -f1 )
   right=$( echo $line | cut -d ">" -f2 )
   goresult=$(go run goSpellcheck.go $wrong | sed "s/'/\'/g")
